4067d45e4ae6236e481f284fb7b8cb94f5848899
Steven Vi <visteven00@gmail.com>
2018-12-20 15:24:37 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Clock from "./Clock";
+import Moment from 'moment-timezone';
 
-const App = (props) => {
-    return (
-        <div>
-            <Clock 
-                icon="sun.svg"
-                timezone={"Sydney/Australia"} 
-                date={new Date()} 
-            />
-        </div>
-    );
+class App extends Component {
+    state = { latitude: null, errorMessage: "", value: 1} //class field react
+    constructor(props) {
+        super(props);
+        // this.state = { latitude: null, errorMessage: "" }; //Initializes the state required
+        
+        
+        window.navigator.geolocation.getCurrentPosition( 
+            position => {this.setState({ latitude: position.coords.latitude })},
+            error => {this.setState({ errorMessage: error.message})}
+        );
+
+        // console.log(position);
+        const tokyoTime = Moment.tz("Asia/Tokyo").format("hh:mm:ss a");
+        console.log(tokyoTime);
+        
+        
+            
+
+    }
+
+    isItWarm() {
+        // return true or false depending on month we're in and latitude you've receive
+        const { latitude } = this.state;
+        if (latitude) {
+            const month = new Date().getMonth();   
+            if ( 
+                (month >= 4 && month <= 9 && latitude > 0 ) 
+                || 
+                ((month >= 9 || month <=4) && latitude < 0)
+                || 
+                (latitude === 0)
+            ) {
+                // console.log('true')
+                return true;
+            }
+                // console.log('false')
+        }
+    }
+
+    getClockIcon() {
+        if(this.isItWarm()) {
+            return "sun.svg"
+        }
+        return "snowflake.svg"
+    };
+
+    render() {
+        // console.log(this.state);
+        const { latitude, errorMessage } = this.state;
+        
+        return (
+                <div>
+                    {this.isItWarm()}
+                    {/* {this.state.latitude} */}
+                    {errorMessage || <Clock 
+                        icon={latitude !== null ? this.getClockIcon() : null}
+                        timezone={"Sydney/Australia"} 
+                        date={new Date()} 
+                    />}
+                </div>
+
+
+
+        );
+    }
 }
 
 export default App;
--- src/Clock.js
+++ src/Clock.js
@@ -1,29 +1,93 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Clock.css";
 
-const Clock = (props) => {
-    const seconds = props.date.getSeconds();
-    const minutes = props.date.getMinutes();
-    const hours = props.date.getHours();
+class Clock extends Component {
     
-    return (
-        <div className="container">
-            <h3 className="label">{props.timezone}</h3>
-            <div className="clock-face" style={{ backgroundImage: `url(/${props.icon})` }}>
+    // Syntactical sugar version
+    state = {
+            seconds: this.props.date.getSeconds(), 
+            minutes: this.props.date.getMinutes(), 
+            hours: this.props.date.getHours()
+        }
+    
+    // Classic way of doing it
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {seconds: this.props.date.getSeconds(), minutes: this.props.date.getMinutes(), hours: this.props.date.getHours()};    
+    // }
+
+    tick() {
+        this.setState({
+            seconds: new Date().getSeconds(),
+            minutes: new Date().getMinutes(),
+            hours: new Date().getHours()
+        });
+    }
+
+    componentDidMount(){
+        console.log("mounted");
+        this.timerID = setInterval(() => {
+            this.tick();
+        }, 1000);
+        console.log(this.timerID);
+    }
+
+    componentDidUpdate(){
+        console.log("update");
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.timeID);
+        console.log("unmounted");
+    }
+
+    render() {
+        return(
+            <div className="container">
+            <h3 className="label">{this.props.timezone}</h3>
+            <div className="clock-face" style={{ backgroundImage: `url(/${this.props.icon})` }}>
                 <div className="clock">
                     <div className="hours-container">
-                        <div className="hours" style={{ transform: `rotateZ(${(hours * 30) + (minutes / 2)}deg)`}}></div>
+                        <div className="hours" style={{ transform: `rotateZ(${(this.state.hours * 30) + (this.state.minutes / 2)}deg)`}}></div>
                     </div>
                     <div className="minutes-container">
-                        <div className="minutes" style={{ transform: `rotateZ(${(minutes * 6)}deg)`}}></div>
+                        <div className="minutes" style={{ transform: `rotateZ(${(this.state.minutes * 6)}deg)`}}></div>
                     </div>
                     <div className="seconds-container">
-                        <div className="seconds" style={{ transform: `rotateZ(${(seconds * 6)}deg)`}}></div>
+                            <div className="seconds" style={{ transform: `rotateZ(${(this.state.seconds * 6)}deg)`}}></div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
+
+
+
+// const Clock = (props) => {
+//     const seconds = props.date.getSeconds();
+//     const minutes = props.date.getMinutes();
+//     const hours = props.date.getHours();
+    
+//     return (
+//         <div className="container">
+//             <h3 className="label">{props.timezone}</h3>
+//             <div className="clock-face" style={{ backgroundImage: `url(/${props.icon})` }}>
+//                 <div className="clock">
+//                     <div className="hours-container">
+//                         <div className="hours" style={{ transform: `rotateZ(${(hours * 30) + (minutes / 2)}deg)`}}></div>
+//                     </div>
+//                     <div className="minutes-container">
+//                         <div className="minutes" style={{ transform: `rotateZ(${(minutes * 6)}deg)`}}></div>
+//                     </div>
+//                     <div className="seconds-container">
+//                         <div className="seconds" style={{ transform: `rotateZ(${(seconds * 6)}deg)`}}></div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
 
 export default Clock;
